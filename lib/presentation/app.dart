1c09efe8059f7c0ff30a22167fbcc26a5acd7a16
farhana-95 <farhanamimi42@gmail.com>
2025-04-22 21:57:03 +0600

    added clean architecture

---
+++ lib/presentation/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
