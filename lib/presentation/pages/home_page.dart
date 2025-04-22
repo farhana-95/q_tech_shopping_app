f8eaa49854b73a0c3df6de511fd7083c81152da5
farhana-95 <farhanamimi42@gmail.com>
2025-04-23 00:32:20 +0600

    added search bar

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -10,10 +10,48 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('E-Commerce App'),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 70,
+        title: Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 44,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.grey[300]!, width: 1),
+                  ),
+                  child: TextField(
+                    onChanged: (value) {},
+                    decoration: InputDecoration(
+                      hintText: 'Search Anything...',
+                      hintStyle: TextStyle(color: Colors.grey[700], fontWeight: FontWeight.w400, fontSize: 16),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Image.asset(
+                          "assets/images/search-normal.png",
+                          height: 18,
+                          width: 18,
+                        ),
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.symmetric(vertical: 12),
+                    ),
+                  ),
+                ),
+              ),
+              // IconButton(
+              //   icon: const Icon(Icons.menu),
+              //   onPressed: () {},
+              // ),
+            ],
+          ),
+        ),
       ),
       body: Column(
         children: [
